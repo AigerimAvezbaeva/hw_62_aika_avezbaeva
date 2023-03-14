a293e0769159c4c586d8052eaad7a01e72543cf5
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-03-15 02:55:11 +0600

    добавила слэш

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from accounts.views import LoginView, logout_view
 
 urlpatterns = [
-    path('login', LoginView.as_view(), name='login'),
-    path('logout', logout_view, name='logout')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout')
 ]
