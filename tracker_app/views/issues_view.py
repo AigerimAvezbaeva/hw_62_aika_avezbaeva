98aa8d541ab188f33712bd184afe6d2b04fbf179
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-03-21 18:40:42 +0600

    сделала необязательным заполнение проекта

--- tracker_app/views/issues_view.py
+++ tracker_app/views/issues_view.py
@@ -1,10 +1,14 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 from django.urls import reverse, reverse_lazy
-from django.views.generic import TemplateView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 from tracker_app.models.issues import Issue
 
 from tracker_app.forms import IssueForm
+
+
+class GetPermissionMixin(PermissionRequiredMixin, UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name__in=['Капитан (Team Lead)', 'Менеджер проекта (Project Manager)', 'Разработчик (Developer)']).exists()
 
 
 class IssueDetail(DetailView):
@@ -12,26 +16,30 @@ class IssueDetail(DetailView):
     model = Issue
 
 
-class IssueAddView(LoginRequiredMixin, CreateView):
+class IssueAddView(GetPermissionMixin, CreateView):
     template_name = 'add_issue.html'
     model = Issue
     form_class = IssueForm
+    permission_required = 'tracker_app.change_issue'
 
     def get_success_url(self):
         return reverse('issue_detail', kwargs={'pk': self.object.pk})
 
 
-class IssueUpdateView(LoginRequiredMixin, UpdateView):
+class IssueUpdateView(GetPermissionMixin, UpdateView):
     template_name = 'update_issue.html'
     model = Issue
     form_class = IssueForm
 
+    def test_func(self):
+        return self.request.user.groups.filter(
+            name__in=['Капитан (Team Lead)', 'Менеджер проекта (Project Manager)', 'Разработчик (Developer)']).exists()
+
     def get_success_url(self):
         return reverse('issue_detail', kwargs={'pk': self.object.pk})
 
 
-class IssueDeleteView(LoginRequiredMixin, DeleteView):
+class IssueDeleteView(GetPermissionMixin, DeleteView):
     template_name = 'delete_issue.html'
     model = Issue
     success_url = reverse_lazy('index')
-
